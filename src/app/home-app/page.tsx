eba15dd6dc0812d951b451f521769805acb92b82
Venkatesh Jayaraman <venkatesh.j@bankbazaar.com>
2023-01-05 11:43:35 +0530

    Enabling app directory and adding a new route called home-app

---
+++ src/app/home-app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "semantic-ui-react";
+
+function HomePage() {
+  return (
+    <>
+      <div>Welcome to Next.js!</div>
+      <Button>Submit</Button>
+    </>
+  );
+}
+
+export default HomePage
+
